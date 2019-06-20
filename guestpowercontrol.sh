bdf85c9fd7d98961acabb93d656f5ea9b729248c
fueledbyb <fueledbyb.py@gmail.com>
2019-06-20 15:37:21 -0400

    Fixed variable names

--- guestpowercontrol.sh
+++ guestpowercontrol.sh
@@ -5,10 +5,10 @@ echo "What is the name of the VM you would like to poweroff?"
 vboxmanage list runningvms
 read NAME
 
-if [[ $SELECT = "soft" ]]; then
+if [[ $OPTION = "soft" ]]; then
   vboxmanage controlvm $NAME poweroff soft
-elif [[ $SELECT = "hard" ]]; then
+elif [[ $OPTION = "hard" ]]; then
     vboxmanage controlvm $NAME poweroff hard
-elif [[ $SELECT = "natural" ]]; then
+elif [[ $OPTION = "natural" ]]; then
   VBoxManage controlvm $NAME acpipowerbutton
 fi
